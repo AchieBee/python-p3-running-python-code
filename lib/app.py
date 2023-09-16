ea7d1fd6294c5efc56bc6c842218aa8bb3a72a6a
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-09-16 11:50:40 +0300

    complete assignment

---
+++ lib/app.py
@@ -0,0 +1,15 @@
+# lib/app.py
+print("Hello world!")
+
+# lib/app.py
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+
+# lib/app.py
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+
+# app.py
+print("Hello World! Pass this test, please.")
